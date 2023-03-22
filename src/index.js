7cea342ed6ed5e5374ac2db7e62f8837a2d327f9
OleksandrNovyk <novykav@gmail.com>
2023-03-22 14:53:52 +0200

    src/index.js

--- src/index.js
+++ src/index.js
@@ -5,69 +5,71 @@ import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
+document.body.style.background = '#ece5da';
+
 const searchEl = document.querySelector('#search-box');
 const countryInfo = document.querySelector('.country-info');
 const countryList = document.querySelector('.country-list');
 
 const cleanMarkup = ref => (ref.innerHTML = '');
 
-const inputHandler = e => {
-  const textInput = e.target.value.trim();
+const inputHandler = evt => {
+const textInput = evt.target.value.trim();
 
-  if (!textInput) {
+if (!textInput) {
     cleanMarkup(countryList);
     cleanMarkup(countryInfo);
     return;
-  }
+}
 
-  fetchCountries(textInput)
+fetchCountries(textInput)
     .then(data => {
-      console.log(data);
-      if (data.length > 10) {
+    console.log(data);
+    if (data.length > 10) {
         Notify.info(
-          'Too many matches found. Please enter a more specific name'
+        'Too many matches found. Please enter a more specific name'
         );
         return;
-      }
-      renderMarkup(data);
+    }
+    renderMarkup(data);
     })
     .catch(err => {
-      cleanMarkup(countryList);
-      cleanMarkup(countryInfo);
-      Notify.failure('Oops, there is no country with that name');
+    cleanMarkup(countryList);
+    cleanMarkup(countryInfo);
+    Notify.failure('Oops, there is no country with that name');
     });
 };
 
 const renderMarkup = data => {
-  if (data.length === 1) {
+if (data.length === 1) {
     cleanMarkup(countryList);
     const markupInfo = createInfoMarkup(data);
     countryInfo.innerHTML = markupInfo;
-  } else {
+} else {
     cleanMarkup(countryInfo);
     const markupList = createListMarkup(data);
     countryList.innerHTML = markupList;
-  }
+}
 };
 
 const createListMarkup = data => {
-  return data
+return data
     .map(
-      ({ name, flags }) =>
+({ name, flags }) =>
         `<li><img src="${flags.png}" alt="${name.official}" width="60" height="40">${name.official}</li>`
     )
     .join('');
 };
 
 const createInfoMarkup = data => {
-  return data.map(
+return data.map(
     ({ name, capital, population, flags, languages }) =>
-      `<img src="${flags.png}" alt="${name.official}" width="200" height="100">
-      <h1>${name.official}</h1>
-      <p>Capital: ${capital}</p>
-      <p>Population: ${population}</p>
-      <p>Languages: ${Object.values(languages)}</p>`
-  );
+`<img src="${flags.png}" alt="${name.official}" width="580" height="300">
+<h1>${name.official}</h1>
+<p>Capital: ${capital}</p>
+<p>Population: ${population}</p>
+<p>Languages: ${Object.values(languages)}</p>`
+);
 };
 
 searchEl.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
