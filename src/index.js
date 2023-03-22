f55d9ecb0361f30f6e05a62c84e76ebb04230fab
OleksandrNovyk <novykav@gmail.com>
2023-03-22 17:30:55 +0200

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
-
-
 const searchEl = document.querySelector('#search-box');
 const countryInfo = document.querySelector('.country-info');
 const countryList = document.querySelector('.country-list');
